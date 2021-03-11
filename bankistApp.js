fdd98e40c1b9a9b71751810974d727649a78f4a2
b3-bogusz <b3.bogusz@gmail.com>
2021-03-11 22:53:47 +0100

    add login, display UI after login

--- bankistApp.js
+++ bankistApp.js
@@ -71,35 +71,32 @@ const calcDisplayMovements = function (movements) {
        containerMovements.insertAdjacentHTML('afterbegin', html);
     })
 }
-calcDisplayMovements(account1.movements);
 
 
 const calcPrintBalance = function (movements) {
     const balance = movements.reduce((acc, mov) => acc + mov, 0);
     labelBalance.textContent = `${balance}€`;
 }
-calcPrintBalance(account1.movements);
 
 
-const calcDisplaySummary = function (movements){
-    const incomes = movements
+const calcDisplaySummary = function (acc){
+    const incomes = acc.movements
         .filter(mov => mov > 0)
         .reduce((acc, mov) => acc + mov, 0);
     labelSumIn.textContent = `${incomes}€`;
 
-    const out = movements
+    const out = acc.movements
         .filter(mov => mov < 0)
         .reduce((acc, mov) => acc + mov, 0);
     labelSumOut.textContent = `${Math.abs(out)}€`;
 
-    const interest = movements
+    const interest = acc.movements
         .filter(mov => mov > 0)
-        .map(deposit => deposit * 1.2/100)
+        .map(deposit => (deposit * acc.interestRate)/100)
         .filter(int => int >= 1)
         .reduce((acc, int) => acc + int, 0);
     labelSumInterest.textContent = `${interest}€`;
 }
-calcDisplaySummary(account1.movements);
 
 
 const createUsernames = accounts => {
@@ -116,3 +113,27 @@ const createUsernames = accounts => {
     })
 }
 createUsernames(accounts);
+
+
+// EVENT HANDLERS
+let currentAccount;
+
+btnLogin.addEventListener('click', function (e) {
+    // Prevent form from submitting
+    e.preventDefault();
+
+    currentAccount = accounts.find(acc => acc.username === inputLoginUsername.value);
+    if (currentAccount?.pin === Number(inputLoginPin.value)){
+        // Display UI and welcome message
+        labelWelcome.textContent = `Welcome back, ${currentAccount.owner.split(' ')[0]}`;
+        containerApp.style.opacity = 1;
+        // Clear input fields
+        inputLoginUsername.value = inputLoginPin.value = '';
+        // Display movements
+        calcDisplayMovements(currentAccount.movements);
+        // Display balance
+        calcPrintBalance(currentAccount.movements);
+        // Display summary
+        calcDisplaySummary(currentAccount);
+    }
+})
