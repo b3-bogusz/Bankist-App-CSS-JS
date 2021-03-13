98abcddb3d2c19408aafba09a041dd72b7147792
b3-bogusz <b3.bogusz@gmail.com>
2021-03-13 19:02:49 +0100

    add sort movements functionality

--- bankistApp.js
+++ bankistApp.js
@@ -59,9 +59,12 @@ const inputCloseUsername = document.querySelector('.form__input--user');
 const inputClosePin = document.querySelector('.form__input--pin');
 
 
-const calcDisplayMovements = function (movements) {
+const calcDisplayMovements = function (movements, sort = false) {
     containerMovements.innerHTML = '';
-    movements.forEach(function (mov, i) {
+
+    const movs = sort ? movements.slice().sort((a, b) => a - b) : movements;
+
+    movs.forEach(function (mov, i) {
         const type = mov > 0 ? 'deposit' : 'withdrawal';
         const html = `
              <div class="movements__row">
@@ -193,3 +196,13 @@ btnClose.addEventListener('click', function (e) {
     }
     inputCloseUsername.value = inputClosePin.value = '';
 })
+
+
+let sorted = false;
+btnSort.addEventListener('click', function (e) {
+    // Prevent form from submitting
+    e.preventDefault();
+
+    calcDisplayMovements(currentAccount.movements, !sorted);
+    sorted = !sorted;
+});
